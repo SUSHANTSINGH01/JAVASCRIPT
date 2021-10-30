6f2e5200935b78371a5d2957c3ce4fd26b651bb1
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-10-31 01:14:08 +0530

    Create arithmetic_operator.js

---
+++ javascript_basic/arithmetic_operator.js
@@ -0,0 +1,44 @@
+
+    var linebreak ="\n";
+	var a = 33;
+	var b = 10;
+	var c = "Test";
+    console.log(linebreak);
+	console.log("a is : ",a);
+    
+    console.log(linebreak);
+    console.log("b is : ",b);
+
+    console.log(linebreak);
+    console.log("c is : ",c);
+
+    console.log(linebreak);
+    result = a + b;
+	console.log("a + b = ",result);
+	console.log(linebreak);
+	
+    result = a - b;
+    console.log("a - b = ",result);
+	console.log(linebreak);
+	
+    result = a / b;
+    console.log("a / b = ",result);
+	console.log(linebreak);
+	
+    result = a % b;
+	console.log("a % b = ",result);
+	console.log(linebreak);
+	
+    result = a + b + c;
+    console.log("a + b + c = ",result);
+	console.log(linebreak);
+	
+    a = a++;
+    result = a++;
+	console.log("a++ = ",result);
+	console.log(linebreak);
+	
+    b = b--;
+    result = b--;
+	console.log("b-- = ",result);
+	console.log(linebreak);

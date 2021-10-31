25d595ce446366dcf36850e1ed9dd202906241fa
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-10-31 18:06:25 +0530

    Create loops.js

---
+++ javascript_basic/loops.js
@@ -0,0 +1,33 @@
+// while loop
+var c=0;
+console.log("\nStarting of while Loop ");
+while(c<10)
+{
+    console.log("\nCurrent Count : " + c);
+    c++;
+}
+console.log("\nwhile Loop stopped.\n")
+
+
+
+// do while loop
+var d=0;
+console.log("\nStarting of Do while Loop ");
+do
+{
+    console.log("\nCurrent Count : " + d)
+    d++;
+}while (d < 5);
+    
+console.log("\nDo while Loop stopped.\n");
+
+
+
+// for loop
+var i;
+console.log("\nStarting of for loop ");
+for(i=0;i<10;i++)
+{
+    console.log("\nCurrent Count : " +i);
+}
+console.log("\nfor Loop stopped.\n");

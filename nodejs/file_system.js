ba17d8a7cfee1b5f17f8ac58b457e83a5c37502b
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-07 23:11:55 +0530

    Update file_system.js

--- nodejs/file_system.js
+++ nodejs/file_system.js
@@ -1,12 +1,54 @@
-const fs = require('fs');
+ var fs = require('fs');
 
 
-    fs.writeFile("file_system2.txt", 'hello, Sushant!', err => 
+ // write in file 
+
+ // first way
+    fs.writeFile("file_system.txt", 'hello, Sushant!', err => 
     {
         if(err)
         {
             console.log('error occured when writing to file');
         }
-        
+       
     });
+
+// second way
+/* fs.writeFile("file_system1.txt", 'console.log("done.")', function(err){
+    
+    console.log("Data Saved.");
+}); */
+
+
+
+
+// read a file
+/*fs.readFile('add.js','utf8',function(err, data){
+
+    console.log(data);
+
+})*/
+
+
+
+// append file
+/*fs.appendFile("file_system1.txt",'added content.', function(err){
+    
+    console.log("Data Saved.");
+});*/
+
+
+// delete file
+/*fs.unlink('file_system2.txt',function(err){
+    
+    console.log("deleted.");
+});*/
+
+
+
+
+
+
+
+    
     

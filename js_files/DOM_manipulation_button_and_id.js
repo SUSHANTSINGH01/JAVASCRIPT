524c5025d69b84ef3d7d52a27744ae1dac3a365c
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-08 00:37:14 +0530

    Add files via upload

---
+++ js_files/DOM_manipulation_button_and_id.js
@@ -0,0 +1,10 @@
+
+
+function buttonclick()
+{
+    //document.getElementById("heading2")   or
+    var str = document.getElementById("heading2")
+    .innerHTML = "DOM_manipulation_button_and_id.";
+   // alert("Button clicked.");  or
+      alert(str);
+}
---
+++ js_files/DOM_manipulation_input_box_and_id.js
@@ -0,0 +1,5 @@
+function inputclick()
+{
+    var str = document.getElementById("text1").value;
+    alert("value inside the text box is : " + str);
+}

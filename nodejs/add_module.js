07763cf251d06be158b13efb234aab853e13e2bb
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-07 21:48:26 +0530

    Create add_module.js

---
+++ nodejs/add_module.js
@@ -0,0 +1,13 @@
+var it = require('./add.js');
+
+function greet(name)
+{
+    console.log("hello, " + name + "!");
+}
+
+greet("Sushant Singh");
+
+it.add(10,20);
+console.log("sum of number a and b is : " + it.add(10,20));
+it.substraction(20,10);
+console.log("substract b from a is : " + it.substraction(20,10));

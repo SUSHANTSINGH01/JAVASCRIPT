4d6f8f3a6a525782cbd3c386f5403b4a80ed80e4
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-09 01:32:36 +0530

    Add files via upload

---
+++ js_files/DOM_manipulation_check_and_id_and_value.js
@@ -0,0 +1,15 @@
+function fn1()
+{
+    
+   
+    var rd1 = document.getElementById("rd1");
+    var rd2 = document.getElementById("rd2");
+
+    if(rd1.checked==true)
+    alert("the channel selected is : " + rd1.value);
+    else if(rd2.checked==true)
+    alert("the channel selected is : " + rd2.value);
+    else
+      alert("no channel selected");
+
+}

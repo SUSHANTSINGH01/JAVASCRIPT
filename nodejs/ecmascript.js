81f1dc38023c0c416c13da593f6888f8b9393ecd
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-09 22:55:15 +0530

    Add files via upload

---
+++ nodejs/ecmascript.js
@@ -0,0 +1,6 @@
+ var name = "Sushant";
+ var age = 21;
+
+ console.log("hi %s you are %s years old", name,age);
+ console.log("hi " + name + " you are " + age + " years old");
+ console.log("hi ${name} you are ${age} years old.");

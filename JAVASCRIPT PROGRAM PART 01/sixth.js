512ed14db3789daf07c9ea812112dc5cc0c40b99
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-02-05 00:07:44 +0530

    Add files via upload

---
+++ JAVASCRIPT PROGRAM PART 01/sixth.js
@@ -0,0 +1,14 @@
+// String indexing 
+
+let Name = "sushant";
+
+//  S    u    s   h   a   n   t 
+//  0    1   2   3   4   5   6
+
+// length of string 
+// Name.length 
+
+console.log(Name[0]);
+console.log(Name.length);
+console.log(Name[Name.length-2]);
+
